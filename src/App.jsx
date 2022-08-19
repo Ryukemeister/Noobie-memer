1c0ade23d994fbf4ed8c83876c7ed32b9b72f2ee
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-19 22:24:59 +0530

    Understanding states in react by playing around it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>hi</h1>
+      <Meme />
     </div>
   );
 }
